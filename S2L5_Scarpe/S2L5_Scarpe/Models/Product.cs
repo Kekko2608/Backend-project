cb5f2b274d609a00c101cd94dbbdf763dc698371
Kekko2608 <francesco.pisu.94@tiscali.it>
2024-06-28 16:42:29 +0200

    Update

--- S2L5_Scarpe/S2L5_Scarpe/Models/Product.cs
+++ S2L5_Scarpe/S2L5_Scarpe/Models/Product.cs
@@ -5,13 +5,13 @@ namespace S2L5_Scarpe.Models
     public class Product
     {
         public int Id { get; set; }
-
+        [Display(Name = "Nome")]
         [Required]
         public string Name { get; set; }
-
+        [Display(Name = "Prezzo")]
         [Required]
         public decimal Price { get; set; }
-
+        [Display(Name = "Descrizione")]
         [Required]
         public string Description { get; set; }
 
