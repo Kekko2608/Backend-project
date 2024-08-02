fdb8c9de63c131aee17d795050d55951c7460c02
Kekko2608 <francesco.pisu.94@tiscali.it>
2024-08-02 11:13:24 +0200

    update tasto aggiungi

--- Progetto Pizzeria/Progetto Pizzeria/obj/Debug/net8.0/Progetto Pizzeria.AssemblyInfo.cs
+++ Progetto Pizzeria/Progetto Pizzeria/obj/Debug/net8.0/Progetto Pizzeria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Progetto Pizzeria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff9dfbe5be8a3a8b9a096e0c42c8a32b76d2994")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7907e3aea6cae298ae4a9896f35f6b772c8a352f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Progetto Pizzeria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Progetto Pizzeria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
